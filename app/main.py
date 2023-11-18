1dcf669106369db6d6582d0704f28bf4a61e6d52
Juan Jesús Alejo Sillero <jalesil1210@iesmartinezm.es>
2023-11-18 19:00:17 +0100

    Tests con schemathesis pasados

--- app/main.py
+++ app/main.py
@@ -1,29 +1,30 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from app.calculator import Calculator
+from app.exceptions import MathematicalException
 from app.serializers.operation import Operation
 
 app = FastAPI()
 
-
-@app.post('/maths/calculate')
+@app.post('/maths/calculate', response_model=int, responses={400: {'description': 'Mathematical error'}})
 def calculate(op: Operation) -> int:
     calculator = Calculator()
 
-    result = calculator.calculate(op.op1, op.op2, operation=op.operation)
+    try:
+        result = calculator.calculate(op.op1, op.op2, operation=op.operation)
+    except MathematicalException as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
     return result
 
-
-@app.post('/maths/expression')
+@app.post('/maths/expression', response_model=str)
 def expression(op: Operation) -> str:
     calculator = Calculator()
 
     result = calculator.get_expression(op.op1, op.op2, operation=op.operation)
 
     return result
-
 
 if __name__ == '__main__':
     uvicorn.run('app.main:app', reload=True)
